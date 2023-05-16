c058b1cb636900708e9e7b2589106d9806803640
anafigueiro <114446641+anafigueiro@users.noreply.github.com>
2023-05-16 17:14:19 -0300

    Add files via upload

---
+++ pweb1_aula_php-main/paginas/ContatoForm.php
@@ -0,0 +1,56 @@
+<?php
+ include '../BD.class.php';
+ $conn = new BD();
+
+ if(!empty($_POST)){
+    try {
+
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST['nome'])) {  
+            throw new Exception(" Somente letras e espaços em branco são permitidos. ");
+        }
+        
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            throw new Exception(" Formato de e-mail inválido. ");
+        }
+        $conn->inserir($_POST);
+        header("location: ContatoList.php");
+
+    } catch (Exception $e){
+        echo $e->getMessage();
+    }
+
+ }
+
+ if(!empty($_GET['id'])){
+    $data = $conn->buscar($_GET['id']);
+    //var_dump($data);
+    //echo "teste";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ContatoForm.php" method="post">
+        <h3>Formulário Contato</h3>
+        <input type="hidden" name="id" value="<?php echo(!empty($data->id)? $data->id : "")?>"><br>
+        <label for="">Nome</label>
+        <input type="text" name="nome" value="<?php echo(!empty($data->nome)? $data->nome : "")?>"><br>
+
+        <label for="">Email</label>
+        <input type="text" name="email" id=""><?php echo(!empty($data->email)? $data->email : "")?><br>
+
+        <label for="">Telefone</label>
+        <input type="text" name="telefone" id=""><?php echo(!empty($data->telefone)? $data->telefone : "")?><br>
+
+        <button type="submit" value="Enviar"><?php echo(empty($_GET['id'])? "Salvar" : "Atualizar")?></button><br>
+        <a href="ContatoList.php">Voltar</a><br><br>
+    </form>
+</body>
+</html>
